b6965e7c23de4e165f49086f451f4460e3ff475d
fabian <chapulinozeta@gmail.com>
2023-04-18 15:27:06 -0400

    deploying

--- api/src/db.js
+++ api/src/db.js
@@ -9,9 +9,6 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-        statement_timeout: 3600000 // 30 seconds
-  },
   port:7799,
   dialect:"postgres"
 });

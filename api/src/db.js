7eb0f578c02a663b368c6e133d0883a82cb78fb1
fabian <chapulinozeta@gmail.com>
2023-04-18 07:58:23 -0400

    Defined a connect timeout for sequelize connection

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,9 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    connectTimeout: 60000 // 30 seconds
+  }
 });
 const basename = path.basename(__filename);
 
